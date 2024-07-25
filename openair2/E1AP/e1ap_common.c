alainrollejr/my_oai_mirror

F1 and E1 changes to send DRB priority

openair2/E1AP/e1ap_common.c
@@ -243,3 +243,62 @@ int e1ap_encode_send(E1_t type, sctp_assoc_t assoc_id, E1AP_E1AP_PDU_t *pdu, uin
 
   return encoded;
 }
+
+uint64_t get_5QI_id(uint64_t fiveqi)
+{
+  for (int id = 0; id < STANDARIZED_5QI_NUM; id++) {
+    if (params_5QI[id].five_QI == fiveqi)
+      return id;
+  }
+  AssertFatal(1 == 0, "Invalid 5QI value\n");
+}
+
+static int qos_cmp(const void *a, const void *b, void *qostype)
+{
+  fiveQI_type_t qos_type = *((fiveQI_type_t *)qostype);
+  long priority1 = 0, priority2 = 0;
+  if (qos_type == non_dynamic) { // non-dynamic
+    priority1 =
+        params_5QI[get_5QI_id(((qos_flow_to_setup_t *)a)->qos_params.qos_characteristics.non_dynamic.fiveqi)].priority_level;
+    priority2 =
+        params_5QI[get_5QI_id(((qos_flow_to_setup_t *)b)->qos_params.qos_characteristics.non_dynamic.fiveqi)].priority_level;
+  } else { // dynamic
+    priority1 = ((qos_flow_to_setup_t *)a)->qos_params.qos_characteristics.dynamic.qos_priority_level;
+    priority2 = ((qos_flow_to_setup_t *)b)->qos_params.qos_characteristics.dynamic.qos_priority_level;
+  }
+  return priority1 - priority2;
+}
+
+void get_drb_characteristics(qos_flow_to_setup_t *qos_flows_in,
+                             int num_qos_flows,
+                             fiveQI_type_t qos_type,
+                             qos_flow_level_qos_parameters_t *dRB_QoS)
+{
+  qos_characteristics_t *qos_char_drb = &dRB_QoS->qos_characteristics;
+  qos_flow_to_setup_t *qos_flow = qos_flows_in;
+
+  if (qos_type == non_dynamic) { // non-dynamic
+    qsort_r(qos_flow, num_qos_flows, sizeof(qos_flow_to_setup_t), qos_cmp, (void *)&qos_type);
+    qos_char_drb->qos_type = non_dynamic;
+    qos_char_drb->non_dynamic.fiveqi = qos_flow->qos_params.qos_characteristics.non_dynamic.fiveqi;
+  } else { // dynamic
+    qsort_r(qos_flow, num_qos_flows, sizeof(qos_flow_to_setup_t), qos_cmp, (void *)&qos_type);
+    qos_char_drb->qos_type = dynamic;
+    qos_char_drb->dynamic.qos_priority_level = qos_flow->qos_params.qos_characteristics.dynamic.qos_priority_level;
+    qos_char_drb->dynamic.packet_delay_budget = qos_flow->qos_params.qos_characteristics.dynamic.packet_delay_budget;
+    qos_char_drb->dynamic.packet_error_rate.per_exponent =
+        qos_flow->qos_params.qos_characteristics.dynamic.packet_error_rate.per_exponent;
+    qos_char_drb->dynamic.packet_error_rate.per_scalar =
+        qos_flow->qos_params.qos_characteristics.dynamic.packet_error_rate.per_scalar;
+  }
+
+  /* GBR Information for a DRB */
+  gbr_qos_flow_information_t *gbr_in = qos_flow->qos_params.gbr_qos_flow_info;
+  if (gbr_in) {
+    dRB_QoS->gbr_qos_flow_info = calloc(1, sizeof(gbr_qos_flow_information_t));
+    dRB_QoS->gbr_qos_flow_info->guar_flow_bit_rate_dl = gbr_in->guar_flow_bit_rate_dl;
+    dRB_QoS->gbr_qos_flow_info->guar_flow_bit_rate_ul = gbr_in->guar_flow_bit_rate_ul;
+    dRB_QoS->gbr_qos_flow_info->max_flow_bit_rate_dl = gbr_in->max_flow_bit_rate_dl;
+    dRB_QoS->gbr_qos_flow_info->max_flow_bit_rate_ul = gbr_in->max_flow_bit_rate_ul;
+  }
+}

openair2/F1AP/f1ap_du_ue_context_management.c
@@ -39,9 +39,9 @@
 #include "openair2/LAYER2/NR_MAC_gNB/nr_mac_gNB.h"
 #include "openair2/LAYER2/nr_pdcp/nr_pdcp_oai_api.h"
 
-static void f1ap_read_drb_qos_param(const F1AP_QoSFlowLevelQoSParameters_t *asn1_qos, f1ap_qos_flow_level_qos_parameters_t *drb_qos)
+static void f1ap_read_drb_qos_param(const F1AP_QoSFlowLevelQoSParameters_t *asn1_qos, qos_flow_level_qos_parameters_t *drb_qos)
 {
-  f1ap_qos_characteristics_t *drb_qos_char = &drb_qos->qos_characteristics;
+  qos_characteristics_t *drb_qos_char = &drb_qos->qos_characteristics;
   const F1AP_QoS_Characteristics_t *dRB_QoS_Char = &asn1_qos->qoS_Characteristics;
 
   if (dRB_QoS_Char->present == F1AP_QoS_Characteristics_PR_non_Dynamic_5QI) {
@@ -64,6 +64,15 @@ static void f1ap_read_drb_qos_param(const F1AP_QoSFlowLevelQoSParameters_t *asn1
   drb_qos->alloc_reten_priority.priority_level = asn1_qos->nGRANallocationRetentionPriority.priorityLevel;
   drb_qos->alloc_reten_priority.preemption_vulnerability = asn1_qos->nGRANallocationRetentionPriority.pre_emptionVulnerability;
   drb_qos->alloc_reten_priority.preemption_capability = asn1_qos->nGRANallocationRetentionPriority.pre_emptionVulnerability;
+
+  /* gbr qos information */
+  if (asn1_qos->gBR_QoS_Flow_Information) {
+    asn1cCalloc(drb_qos->gbr_qos_flow_info, tmp);
+    asn_INTEGER2long(&asn1_qos->gBR_QoS_Flow_Information->guaranteedFlowBitRateDownlink, &tmp->guar_flow_bit_rate_dl);
+    asn_INTEGER2long(&asn1_qos->gBR_QoS_Flow_Information->guaranteedFlowBitRateUplink, &tmp->guar_flow_bit_rate_ul);
+    asn_INTEGER2long(&asn1_qos->gBR_QoS_Flow_Information->maxFlowBitRateDownlink, &tmp->max_flow_bit_rate_dl);
+    asn_INTEGER2long(&asn1_qos->gBR_QoS_Flow_Information->maxFlowBitRateUplink, &tmp->max_flow_bit_rate_ul);
+  }
 }
 
 static void f1ap_read_flows_mapped(const F1AP_Flows_Mapped_To_DRB_List_t *asn1_flows_mapped, f1ap_flows_mapped_to_drb_t *flows_mapped, int n)
@@ -76,11 +85,11 @@ static void f1ap_read_flows_mapped(const F1AP_Flows_Mapped_To_DRB_List_t *asn1_f
 
     /* QoS-Flow-Level-QoS-Parameters */
     {
-      f1ap_qos_flow_level_qos_parameters_t *flow_qos = &flows_mapped_to_drb->qos_params;
+      qos_flow_level_qos_parameters_t *flow_qos = &flows_mapped_to_drb->qos_params;
       const F1AP_QoSFlowLevelQoSParameters_t *Flow_QoS = &flows_Mapped_To_Drb->qoSFlowLevelQoSParameters;
 
       /* QoS Characteristics*/
-      f1ap_qos_characteristics_t *flow_qos_char = &flow_qos->qos_characteristics;
+      qos_characteristics_t *flow_qos_char = &flow_qos->qos_characteristics;
       const F1AP_QoS_Characteristics_t *Flow_QoS_Char = &Flow_QoS->qoS_Characteristics;
 
       if (Flow_QoS_Char->present == F1AP_QoS_Characteristics_PR_non_Dynamic_5QI) {
@@ -103,6 +112,15 @@ static void f1ap_read_flows_mapped(const F1AP_Flows_Mapped_To_DRB_List_t *asn1_f
       flow_qos->alloc_reten_priority.priority_level = Flow_QoS->nGRANallocationRetentionPriority.priorityLevel;
       flow_qos->alloc_reten_priority.preemption_vulnerability = Flow_QoS->nGRANallocationRetentionPriority.pre_emptionVulnerability;
       flow_qos->alloc_reten_priority.preemption_capability = Flow_QoS->nGRANallocationRetentionPriority.pre_emptionVulnerability;
+
+      /* gbr qos information */
+      if (Flow_QoS->gBR_QoS_Flow_Information) {
+        asn1cCalloc(flow_qos->gbr_qos_flow_info, tmp);
+        asn_INTEGER2long(&Flow_QoS->gBR_QoS_Flow_Information->guaranteedFlowBitRateDownlink, &tmp->guar_flow_bit_rate_dl);
+        asn_INTEGER2long(&Flow_QoS->gBR_QoS_Flow_Information->guaranteedFlowBitRateUplink, &tmp->guar_flow_bit_rate_ul);
+        asn_INTEGER2long(&Flow_QoS->gBR_QoS_Flow_Information->maxFlowBitRateDownlink, &tmp->max_flow_bit_rate_dl);
+        asn_INTEGER2long(&Flow_QoS->gBR_QoS_Flow_Information->maxFlowBitRateUplink, &tmp->max_flow_bit_rate_ul);
+      }
     }
   }
 }

openair2/LAYER2/NR_MAC_gNB/mac_rrc_dl_handler.c
@@ -24,6 +24,7 @@
 #include "mac_proto.h"
 #include "openair2/F1AP/f1ap_ids.h"
 #include "openair2/F1AP/f1ap_common.h"
+#include "openair2/E1AP/e1ap_common.h"
 #include "openair2/LAYER2/nr_rlc/nr_rlc_oai_api.h"
 #include "F1AP_CauseRadioNetwork.h"
 #include "openair3/ocp-gtpu/gtp_itf.h"
@@ -31,11 +32,6 @@
 
 #include "uper_decoder.h"
 #include "uper_encoder.h"
-
-// Standarized 5QI values and Default Priority levels as mentioned in 3GPP TS 23.501 Table 5.7.4-1
-const uint64_t qos_fiveqi[26] = {1, 2, 3, 4, 65, 66, 67, 71, 72, 73, 74, 76, 5, 6, 7, 8, 9, 69, 70, 79, 80, 82, 83, 84, 85, 86};
-const uint64_t qos_priority[26] = {20, 40, 30, 50, 7, 20, 15, 56, 56, 56, 56, 56, 10,
-                                   60, 70, 80, 90, 5, 55, 65, 68, 19, 22, 24, 21, 18};
 
 static instance_t get_f1_gtp_instance(void)
 {
@@ -164,7 +160,7 @@ static int handle_ue_context_srbs_setup(NR_UE_info_t *UE,
     nr_rlc_add_srb(UE->rnti, srb->srb_id, rlc_BearerConfig);
 
     int priority = rlc_BearerConfig->mac_LogicalChannelConfig->ul_SpecificParameters->priority;
-    nr_lc_config_t c = {.lcid = rlc_BearerConfig->logicalChannelIdentity, .priority = priority};
+    nr_lc_config_t c = {.lcid = rlc_BearerConfig->logicalChannelIdentity, .priority = priority, .guaranteed_bitrate = UINT64_MAX};
     nr_mac_add_lcid(&UE->UE_sched_ctrl, &c);
 
     (*resp_srbs)[i] = *srb;
@@ -182,16 +178,7 @@ static NR_RLC_BearerConfig_t *get_bearerconfig_from_drb(const f1ap_drb_to_be_set
   return get_DRB_RLC_BearerConfig(get_lcid_from_drbid(drb->drb_id), drb->drb_id, rlc_conf, priority);
 }
 
-static int get_non_dynamic_priority(int fiveqi)
-{
-  for (int i = 0; i < sizeofArray(qos_fiveqi); ++i)
-    if (qos_fiveqi[i] == fiveqi)
-      return qos_priority[i];
-  AssertFatal(false, "illegal 5QI value %d\n", fiveqi);
-  return 0;
-}
-
-static NR_QoS_config_t get_qos_config(const f1ap_qos_characteristics_t *qos_char)
+static NR_QoS_config_t get_qos_config(const qos_characteristics_t *qos_char)
 {
   NR_QoS_config_t qos_c = {0};
   switch (qos_char->qos_type) {
@@ -201,7 +188,7 @@ static NR_QoS_config_t get_qos_config(const f1ap_qos_characteristics_t *qos_char
       break;
     case non_dynamic:
       qos_c.fiveQI = qos_char->non_dynamic.fiveqi;
-      qos_c.priority = get_non_dynamic_priority(qos_char->non_dynamic.fiveqi);
+      qos_c.priority = params_5QI[get_5QI_id(qos_c.fiveQI)].priority_level;
       break;
     default:
       AssertFatal(false, "illegal QoS type %d\n", qos_char->qos_type);
@@ -225,6 +212,9 @@ static int handle_ue_context_drbs_setup(NR_UE_info_t *UE,
   AssertFatal(*resp_drbs != NULL, "out of memory\n");
   for (int i = 0; i < drbs_len; i++) {
     const f1ap_drb_to_be_setup_t *drb = &req_drbs[i];
+    const gbr_qos_flow_information_t *gbr_qos_info_drb = drb->drb_info.drb_qos.gbr_qos_flow_info;
+    const qos_characteristics_t *qos_char = &drb->drb_info.drb_qos.qos_characteristics;
+
     f1ap_drb_to_be_setup_t *resp_drb = &(*resp_drbs)[i];
     NR_RLC_BearerConfig_t *rlc_BearerConfig = get_bearerconfig_from_drb(drb);
     nr_rlc_add_drb(UE->rnti, drb->drb_id, rlc_BearerConfig);
@@ -233,9 +223,21 @@ static int handle_ue_context_drbs_setup(NR_UE_info_t *UE,
     int prio = 100;
     for (int q = 0; q < drb->drb_info.flows_to_be_setup_length; ++q) {
       c.qos_config[q] = get_qos_config(&drb->drb_info.flows_mapped_to_drb[q].qos_params.qos_characteristics);
-      prio = min(prio, c.qos_config[q].priority);
     }
+    if (qos_char->qos_type == non_dynamic)
+      prio = params_5QI[get_5QI_id(qos_char->non_dynamic.fiveqi)].priority_level;
+    else
+      prio = qos_char->dynamic.qos_priority_level;
     c.priority = prio;
+
+    int m_bitrate = 0, g_bitrate = 0;
+    if (gbr_qos_info_drb) {
+      g_bitrate = gbr_qos_info_drb->guar_flow_bit_rate_dl;
+      m_bitrate = gbr_qos_info_drb->max_flow_bit_rate_dl;
+    }
+    c.guaranteed_bitrate = g_bitrate;
+    c.max_bitrate = m_bitrate;
+
     nr_mac_add_lcid(&UE->UE_sched_ctrl, &c);
 
     *resp_drb = *drb;

openair2/RRC/NR/rrc_gNB.c
@@ -2162,7 +2162,9 @@ void rrc_gNB_process_e1_bearer_context_setup_resp(e1ap_bearer_setup_resp_t *resp
         drb->drb_info.flows_mapped_to_drb[j].qfi = drb_config->qosFlows[j].qfi;
 
         pdusession_level_qos_parameter_t *in_qos_char = get_qos_characteristics(drb_config->qosFlows[j].qfi, RRC_pduSession);
-        f1ap_qos_characteristics_t *qos_char = &drb->drb_info.flows_mapped_to_drb[j].qos_params.qos_characteristics;
+        qos_flow_level_qos_parameters_t *qos_params = &drb->drb_info.flows_mapped_to_drb[j].qos_params;
+        qos_characteristics_t *qos_char = &qos_params->qos_characteristics;
+
         if (in_qos_char->fiveQI_type == dynamic) {
           qos_char->qos_type = dynamic;
           qos_char->dynamic.fiveqi = in_qos_char->fiveQI;
@@ -2172,9 +2174,18 @@ void rrc_gNB_process_e1_bearer_context_setup_resp(e1ap_bearer_setup_resp_t *resp
           qos_char->non_dynamic.fiveqi = in_qos_char->fiveQI;
           qos_char->non_dynamic.qos_priority_level = in_qos_char->qos_priority;
         }
+        asn1cCalloc(qos_params->gbr_qos_flow_info, gbr_info);
+        gbr_info->guar_flow_bit_rate_dl = in_qos_char->gbr_qos_flow_level_qos_params.guar_flow_bit_rate_dl;
+        gbr_info->guar_flow_bit_rate_ul = in_qos_char->gbr_qos_flow_level_qos_params.guar_flow_bit_rate_ul;
+        gbr_info->max_flow_bit_rate_dl = in_qos_char->gbr_qos_flow_level_qos_params.max_flow_bit_rate_dl;
+        gbr_info->max_flow_bit_rate_ul = in_qos_char->gbr_qos_flow_level_qos_params.max_flow_bit_rate_ul;
       }
+
       /* the DRB QoS parameters: we just reuse the ones from the first flow */
-      drb->drb_info.drb_qos = drb->drb_info.flows_mapped_to_drb[0].qos_params;
+      get_drb_characteristics((qos_flow_to_setup_t *)drb->drb_info.flows_mapped_to_drb,
+                              drb->drb_info.flows_to_be_setup_length,
+                              drb->drb_info.flows_mapped_to_drb[0].qos_params.qos_characteristics.qos_type,
+                              &drb->drb_info.drb_qos);
 
       /* pass NSSAI info to MAC */
       drb->nssai = RRC_pduSession->param.nssai;

openair2/RRC/NR/rrc_gNB_NGAP.c
@@ -440,9 +440,16 @@ void trigger_bearer_setup(gNB_RRC_INST *rrc, gNB_RRC_UE_t *UE, int n, pdusession
 
         ngran_allocation_retention_priority_t *rent_priority = &qos_flow->qos_params.alloc_reten_priority;
         ngap_allocation_retention_priority_t *rent_priority_in = &qos_session->allocation_retention_priority;
-        rent_priority->priority_level = rent_priority_in->priority_level;
-        rent_priority->preemption_capability = rent_priority_in->pre_emp_capability;
-        rent_priority->preemption_vulnerability = rent_priority_in->pre_emp_vulnerability;
+        rent_priority->priority_level = (uint16_t)rent_priority_in->priority_level;
+        rent_priority->preemption_capability = (preemption_capability_t)((uint16_t)rent_priority_in->pre_emp_capability);
+        rent_priority->preemption_vulnerability = (preemption_vulnerability_t)((uint16_t)rent_priority_in->pre_emp_vulnerability);
+
+        /* todo: should be modified here for gbr info of the qos flow (temporary fix) */
+        if ((qos_char->qos_type == non_dynamic && qos_session->fiveQI < 5) || qos_char->qos_type == dynamic) {
+          qos_session->gbr_qos_flow_level_qos_params.guar_flow_bit_rate_dl = (long)ueAggMaxBitRateDownlink;
+          asn1cCalloc(qos_flow->qos_params.gbr_qos_flow_info, gbr_ie);
+          gbr_ie->guar_flow_bit_rate_dl = qos_session->gbr_qos_flow_level_qos_params.guar_flow_bit_rate_dl;
+        }
       }
     }
   }

openaicellular/openairinterface5G

openair2/E1AP/e1ap_common.c
@@ -21,6 +21,7 @@
  *      contact@openairinterface.org
  */
 
+#define _GNU_SOURCE
 #include <time.h>
 #include <stdlib.h>
 #include "e1ap_common.h"
@@ -242,4 +243,63 @@ int e1ap_encode_send(E1_t type, sctp_assoc_t assoc_id, E1AP_E1AP_PDU_t *pdu, uin
   itti_send_msg_to_task(TASK_SCTP, 0 /*unused by callee*/, message);
 
   return encoded;
+}
+
+uint64_t get_5QI_id(uint64_t fiveqi)
+{
+  for (int id = 0; id < STANDARIZED_5QI_NUM; id++) {
+    if (params_5QI[id].five_QI == fiveqi)
+      return id;
+  }
+  AssertFatal(1 == 0, "Invalid 5QI value\n");
+}
+
+static int qos_cmp(const void *a, const void *b, void *qostype)
+{
+  fiveQI_type_t qos_type = *((fiveQI_type_t *)qostype);
+  long priority1 = 0, priority2 = 0;
+  if (qos_type == non_dynamic) { // non-dynamic
+    priority1 =
+        params_5QI[get_5QI_id(((qos_flow_to_setup_t *)a)->qos_params.qos_characteristics.non_dynamic.fiveqi)].priority_level;
+    priority2 =
+        params_5QI[get_5QI_id(((qos_flow_to_setup_t *)b)->qos_params.qos_characteristics.non_dynamic.fiveqi)].priority_level;
+  } else { // dynamic
+    priority1 = ((qos_flow_to_setup_t *)a)->qos_params.qos_characteristics.dynamic.qos_priority_level;
+    priority2 = ((qos_flow_to_setup_t *)b)->qos_params.qos_characteristics.dynamic.qos_priority_level;
+  }
+  return priority1 - priority2;
+}
+
+void get_drb_characteristics(qos_flow_to_setup_t *qos_flows_in,
+                             int num_qos_flows,
+                             fiveQI_type_t qos_type,
+                             qos_flow_level_qos_parameters_t *dRB_QoS)
+{
+  qos_characteristics_t *qos_char_drb = &dRB_QoS->qos_characteristics;
+  qos_flow_to_setup_t *qos_flow = qos_flows_in;
+
+  if (qos_type == non_dynamic) { // non-dynamic
+    qsort_r(qos_flow, num_qos_flows, sizeof(qos_flow_to_setup_t), qos_cmp, (void *)&qos_type);
+    qos_char_drb->qos_type = non_dynamic;
+    qos_char_drb->non_dynamic.fiveqi = qos_flow->qos_params.qos_characteristics.non_dynamic.fiveqi;
+  } else { // dynamic
+    qsort_r(qos_flow, num_qos_flows, sizeof(qos_flow_to_setup_t), qos_cmp, (void *)&qos_type);
+    qos_char_drb->qos_type = dynamic;
+    qos_char_drb->dynamic.qos_priority_level = qos_flow->qos_params.qos_characteristics.dynamic.qos_priority_level;
+    qos_char_drb->dynamic.packet_delay_budget = qos_flow->qos_params.qos_characteristics.dynamic.packet_delay_budget;
+    qos_char_drb->dynamic.packet_error_rate.per_exponent =
+        qos_flow->qos_params.qos_characteristics.dynamic.packet_error_rate.per_exponent;
+    qos_char_drb->dynamic.packet_error_rate.per_scalar =
+        qos_flow->qos_params.qos_characteristics.dynamic.packet_error_rate.per_scalar;
+  }
+
+  /* GBR Information for a DRB */
+  gbr_qos_flow_information_t *gbr_in = qos_flow->qos_params.gbr_qos_flow_info;
+  if (gbr_in) {
+    dRB_QoS->gbr_qos_flow_info = calloc(1, sizeof(gbr_qos_flow_information_t));
+    dRB_QoS->gbr_qos_flow_info->gbr_dl = gbr_in->gbr_dl;
+    dRB_QoS->gbr_qos_flow_info->gbr_ul = gbr_in->gbr_ul;
+    dRB_QoS->gbr_qos_flow_info->mbr_dl = gbr_in->mbr_dl;
+    dRB_QoS->gbr_qos_flow_info->mbr_ul = gbr_in->mbr_ul;
+  }
 }

openair2/F1AP/f1ap_du_ue_context_management.c
@@ -39,9 +39,9 @@
 #include "openair2/LAYER2/NR_MAC_gNB/nr_mac_gNB.h"
 #include "openair2/LAYER2/nr_pdcp/nr_pdcp_oai_api.h"
 
-static void f1ap_read_drb_qos_param(const F1AP_QoSFlowLevelQoSParameters_t *asn1_qos, f1ap_qos_flow_level_qos_parameters_t *drb_qos)
+static void f1ap_read_drb_qos_param(const F1AP_QoSFlowLevelQoSParameters_t *asn1_qos, qos_flow_level_qos_parameters_t *drb_qos)
 {
-  f1ap_qos_characteristics_t *drb_qos_char = &drb_qos->qos_characteristics;
+  qos_characteristics_t *drb_qos_char = &drb_qos->qos_characteristics;
   const F1AP_QoS_Characteristics_t *dRB_QoS_Char = &asn1_qos->qoS_Characteristics;
 
   if (dRB_QoS_Char->present == F1AP_QoS_Characteristics_PR_non_Dynamic_5QI) {
@@ -64,6 +64,15 @@ static void f1ap_read_drb_qos_param(const F1AP_QoSFlowLevelQoSParameters_t *asn1
   drb_qos->alloc_reten_priority.priority_level = asn1_qos->nGRANallocationRetentionPriority.priorityLevel;
   drb_qos->alloc_reten_priority.preemption_vulnerability = asn1_qos->nGRANallocationRetentionPriority.pre_emptionVulnerability;
   drb_qos->alloc_reten_priority.preemption_capability = asn1_qos->nGRANallocationRetentionPriority.pre_emptionVulnerability;
+
+  /* gbr qos information */
+  if (asn1_qos->gBR_QoS_Flow_Information) {
+    asn1cCalloc(drb_qos->gbr_qos_flow_info, tmp);
+    asn_INTEGER2long(&asn1_qos->gBR_QoS_Flow_Information->guaranteedFlowBitRateDownlink, &tmp->gbr_dl);
+    asn_INTEGER2long(&asn1_qos->gBR_QoS_Flow_Information->guaranteedFlowBitRateUplink, &tmp->gbr_ul);
+    asn_INTEGER2long(&asn1_qos->gBR_QoS_Flow_Information->maxFlowBitRateDownlink, &tmp->mbr_dl);
+    asn_INTEGER2long(&asn1_qos->gBR_QoS_Flow_Information->maxFlowBitRateUplink, &tmp->mbr_ul);
+  }
 }
 
 static void f1ap_read_flows_mapped(const F1AP_Flows_Mapped_To_DRB_List_t *asn1_flows_mapped, f1ap_flows_mapped_to_drb_t *flows_mapped, int n)
@@ -76,11 +85,11 @@ static void f1ap_read_flows_mapped(const F1AP_Flows_Mapped_To_DRB_List_t *asn1_f
 
     /* QoS-Flow-Level-QoS-Parameters */
     {
-      f1ap_qos_flow_level_qos_parameters_t *flow_qos = &flows_mapped_to_drb->qos_params;
+      qos_flow_level_qos_parameters_t *flow_qos = &flows_mapped_to_drb->qos_params;
       const F1AP_QoSFlowLevelQoSParameters_t *Flow_QoS = &flows_Mapped_To_Drb->qoSFlowLevelQoSParameters;
 
       /* QoS Characteristics*/
-      f1ap_qos_characteristics_t *flow_qos_char = &flow_qos->qos_characteristics;
+      qos_characteristics_t *flow_qos_char = &flow_qos->qos_characteristics;
       const F1AP_QoS_Characteristics_t *Flow_QoS_Char = &Flow_QoS->qoS_Characteristics;
 
       if (Flow_QoS_Char->present == F1AP_QoS_Characteristics_PR_non_Dynamic_5QI) {
@@ -103,6 +112,15 @@ static void f1ap_read_flows_mapped(const F1AP_Flows_Mapped_To_DRB_List_t *asn1_f
       flow_qos->alloc_reten_priority.priority_level = Flow_QoS->nGRANallocationRetentionPriority.priorityLevel;
       flow_qos->alloc_reten_priority.preemption_vulnerability = Flow_QoS->nGRANallocationRetentionPriority.pre_emptionVulnerability;
       flow_qos->alloc_reten_priority.preemption_capability = Flow_QoS->nGRANallocationRetentionPriority.pre_emptionVulnerability;
+
+      /* gbr qos information */
+      if (Flow_QoS->gBR_QoS_Flow_Information) {
+        asn1cCalloc(flow_qos->gbr_qos_flow_info, tmp);
+        asn_INTEGER2long(&Flow_QoS->gBR_QoS_Flow_Information->guaranteedFlowBitRateDownlink, &tmp->gbr_dl);
+        asn_INTEGER2long(&Flow_QoS->gBR_QoS_Flow_Information->guaranteedFlowBitRateUplink, &tmp->gbr_ul);
+        asn_INTEGER2long(&Flow_QoS->gBR_QoS_Flow_Information->maxFlowBitRateDownlink, &tmp->mbr_dl);
+        asn_INTEGER2long(&Flow_QoS->gBR_QoS_Flow_Information->maxFlowBitRateUplink, &tmp->mbr_ul);
+      }
     }
   }
 }

openair2/LAYER2/NR_MAC_gNB/mac_rrc_dl_handler.c
@@ -24,6 +24,7 @@
 #include "mac_proto.h"
 #include "openair2/F1AP/f1ap_ids.h"
 #include "openair2/F1AP/f1ap_common.h"
+#include "openair2/E1AP/e1ap_common.h"
 #include "openair2/LAYER2/nr_rlc/nr_rlc_oai_api.h"
 #include "F1AP_CauseRadioNetwork.h"
 #include "openair3/ocp-gtpu/gtp_itf.h"
@@ -31,11 +32,6 @@
 
 #include "uper_decoder.h"
 #include "uper_encoder.h"
-
-// Standarized 5QI values and Default Priority levels as mentioned in 3GPP TS 23.501 Table 5.7.4-1
-const uint64_t qos_fiveqi[26] = {1, 2, 3, 4, 65, 66, 67, 71, 72, 73, 74, 76, 5, 6, 7, 8, 9, 69, 70, 79, 80, 82, 83, 84, 85, 86};
-const uint64_t qos_priority[26] = {20, 40, 30, 50, 7, 20, 15, 56, 56, 56, 56, 56, 10,
-                                   60, 70, 80, 90, 5, 55, 65, 68, 19, 22, 24, 21, 18};
 
 static instance_t get_f1_gtp_instance(void)
 {
@@ -164,7 +160,7 @@ static int handle_ue_context_srbs_setup(NR_UE_info_t *UE,
     nr_rlc_add_srb(UE->rnti, srb->srb_id, rlc_BearerConfig);
 
     int priority = rlc_BearerConfig->mac_LogicalChannelConfig->ul_SpecificParameters->priority;
-    nr_lc_config_t c = {.lcid = rlc_BearerConfig->logicalChannelIdentity, .priority = priority};
+    nr_lc_config_t c = {.lcid = rlc_BearerConfig->logicalChannelIdentity, .priority = priority, .guaranteed_bitrate = UINT64_MAX};
     nr_mac_add_lcid(&UE->UE_sched_ctrl, &c);
 
     (*resp_srbs)[i] = *srb;
@@ -182,16 +178,7 @@ static NR_RLC_BearerConfig_t *get_bearerconfig_from_drb(const f1ap_drb_to_be_set
   return get_DRB_RLC_BearerConfig(get_lcid_from_drbid(drb->drb_id), drb->drb_id, rlc_conf, priority);
 }
 
-static int get_non_dynamic_priority(int fiveqi)
-{
-  for (int i = 0; i < sizeofArray(qos_fiveqi); ++i)
-    if (qos_fiveqi[i] == fiveqi)
-      return qos_priority[i];
-  AssertFatal(false, "illegal 5QI value %d\n", fiveqi);
-  return 0;
-}
-
-static NR_QoS_config_t get_qos_config(const f1ap_qos_characteristics_t *qos_char)
+static NR_QoS_config_t get_qos_config(const qos_characteristics_t *qos_char)
 {
   NR_QoS_config_t qos_c = {0};
   switch (qos_char->qos_type) {
@@ -201,7 +188,7 @@ static NR_QoS_config_t get_qos_config(const f1ap_qos_characteristics_t *qos_char
       break;
     case non_dynamic:
       qos_c.fiveQI = qos_char->non_dynamic.fiveqi;
-      qos_c.priority = get_non_dynamic_priority(qos_char->non_dynamic.fiveqi);
+      qos_c.priority = params_5QI[get_5QI_id(qos_c.fiveQI)].priority_level;
       break;
     default:
       AssertFatal(false, "illegal QoS type %d\n", qos_char->qos_type);
@@ -225,6 +212,9 @@ static int handle_ue_context_drbs_setup(NR_UE_info_t *UE,
   AssertFatal(*resp_drbs != NULL, "out of memory\n");
   for (int i = 0; i < drbs_len; i++) {
     const f1ap_drb_to_be_setup_t *drb = &req_drbs[i];
+    const gbr_qos_flow_information_t *gbr_qos_info_drb = drb->drb_info.drb_qos.gbr_qos_flow_info;
+    const qos_characteristics_t *qos_char = &drb->drb_info.drb_qos.qos_characteristics;
+
     f1ap_drb_to_be_setup_t *resp_drb = &(*resp_drbs)[i];
     NR_RLC_BearerConfig_t *rlc_BearerConfig = get_bearerconfig_from_drb(drb);
     nr_rlc_add_drb(UE->rnti, drb->drb_id, rlc_BearerConfig);
@@ -233,9 +223,21 @@ static int handle_ue_context_drbs_setup(NR_UE_info_t *UE,
     int prio = 100;
     for (int q = 0; q < drb->drb_info.flows_to_be_setup_length; ++q) {
       c.qos_config[q] = get_qos_config(&drb->drb_info.flows_mapped_to_drb[q].qos_params.qos_characteristics);
-      prio = min(prio, c.qos_config[q].priority);
     }
+    if (qos_char->qos_type == non_dynamic)
+      prio = params_5QI[get_5QI_id(qos_char->non_dynamic.fiveqi)].priority_level;
+    else
+      prio = qos_char->dynamic.qos_priority_level;
     c.priority = prio;
+
+    int m_bitrate = 0, g_bitrate = 0;
+    if (gbr_qos_info_drb) {
+      g_bitrate = gbr_qos_info_drb->gbr_dl;
+      m_bitrate = gbr_qos_info_drb->mbr_dl;
+    }
+    c.guaranteed_bitrate = g_bitrate;
+    c.max_bitrate = m_bitrate;
+
     nr_mac_add_lcid(&UE->UE_sched_ctrl, &c);
 
     *resp_drb = *drb;

openair2/RRC/NR/rrc_gNB.c
@@ -2179,7 +2179,9 @@ void rrc_gNB_process_e1_bearer_context_setup_resp(e1ap_bearer_setup_resp_t *resp
         drb->drb_info.flows_mapped_to_drb[j].qfi = drb_config->qosFlows[j].qfi;
 
         pdusession_level_qos_parameter_t *in_qos_char = get_qos_characteristics(drb_config->qosFlows[j].qfi, RRC_pduSession);
-        f1ap_qos_characteristics_t *qos_char = &drb->drb_info.flows_mapped_to_drb[j].qos_params.qos_characteristics;
+        qos_flow_level_qos_parameters_t *qos_params = &drb->drb_info.flows_mapped_to_drb[j].qos_params;
+        qos_characteristics_t *qos_char = &qos_params->qos_characteristics;
+
         if (in_qos_char->fiveQI_type == dynamic) {
           qos_char->qos_type = dynamic;
           qos_char->dynamic.fiveqi = in_qos_char->fiveQI;
@@ -2189,9 +2191,18 @@ void rrc_gNB_process_e1_bearer_context_setup_resp(e1ap_bearer_setup_resp_t *resp
           qos_char->non_dynamic.fiveqi = in_qos_char->fiveQI;
           qos_char->non_dynamic.qos_priority_level = in_qos_char->qos_priority;
         }
+        asn1cCalloc(qos_params->gbr_qos_flow_info, gbr_info);
+        gbr_info->gbr_dl = in_qos_char->gbr_qos_flow_level_qos_params.gbr_dl;
+        gbr_info->gbr_ul = in_qos_char->gbr_qos_flow_level_qos_params.gbr_ul;
+        gbr_info->mbr_dl = in_qos_char->gbr_qos_flow_level_qos_params.mbr_dl;
+        gbr_info->mbr_ul = in_qos_char->gbr_qos_flow_level_qos_params.mbr_ul;
       }
+
       /* the DRB QoS parameters: we just reuse the ones from the first flow */
-      drb->drb_info.drb_qos = drb->drb_info.flows_mapped_to_drb[0].qos_params;
+      get_drb_characteristics((qos_flow_to_setup_t *)drb->drb_info.flows_mapped_to_drb,
+                              drb->drb_info.flows_to_be_setup_length,
+                              drb->drb_info.flows_mapped_to_drb[0].qos_params.qos_characteristics.qos_type,
+                              &drb->drb_info.drb_qos);
 
       /* pass NSSAI info to MAC */
       drb->nssai = RRC_pduSession->param.nssai;

openair2/RRC/NR/rrc_gNB_NGAP.c
@@ -422,9 +422,16 @@ void trigger_bearer_setup(gNB_RRC_INST *rrc, gNB_RRC_UE_t *UE, int n, pdusession
 
         ngran_allocation_retention_priority_t *rent_priority = &qos_flow->qos_params.alloc_reten_priority;
         ngap_allocation_retention_priority_t *rent_priority_in = &qos_session->allocation_retention_priority;
-        rent_priority->priority_level = rent_priority_in->priority_level;
-        rent_priority->preemption_capability = rent_priority_in->pre_emp_capability;
-        rent_priority->preemption_vulnerability = rent_priority_in->pre_emp_vulnerability;
+        rent_priority->priority_level = (uint16_t)rent_priority_in->priority_level;
+        rent_priority->preemption_capability = (preemption_capability_t)((uint16_t)rent_priority_in->pre_emp_capability);
+        rent_priority->preemption_vulnerability = (preemption_vulnerability_t)((uint16_t)rent_priority_in->pre_emp_vulnerability);
+
+        /* todo: should be modified here for gbr info of the qos flow (temporary fix) */
+        if ((qos_char->qos_type == non_dynamic && qos_session->fiveQI < 5) || qos_char->qos_type == dynamic) {
+          qos_session->gbr_qos_flow_level_qos_params.gbr_dl = (long)ueAggMaxBitRateDownlink;
+          asn1cCalloc(qos_flow->qos_params.gbr_qos_flow_info, gbr_ie);
+          gbr_ie->gbr_dl = qos_session->gbr_qos_flow_level_qos_params.gbr_dl;
+        }
       }
     }
   }